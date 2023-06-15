d79874e9b2a026a885d6f235482fa84f914cd87d
AccessiT3ch <accessiT3ch@gmail.com>
2023-06-15 16:17:11 -0700

    Add github workflow next config options

--- packages/preview/next.config.js
+++ packages/preview/next.config.js
@@ -4,6 +4,12 @@ const path = require("path");
 
 const prod = process.env.NODE_ENV === "production";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+const repoName = isGithubActions
+  ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+  : "";
+
 module.exports = withPWA({
   disable: !prod,
   dest: "public",
@@ -32,6 +38,6 @@ module.exports = withPWA({
     config.resolve.alias["@utils"] = path.join(__dirname, `src/utils`);
     return config;
   },
-  assetPrefix: process.env.BASE_PATH || "",
-  basePath: process.env.BASE_PATH || "",
+  assetPrefix: process.env.BASE_PATH || (isGithubActions ? "/" + repoName : ""),
+  basePath: process.env.BASE_PATH || (isGithubActions ? "/" + repoName : ""),
 });
